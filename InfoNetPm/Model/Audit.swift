583efca5dbc242acfcc5fb6cc07830fd0bcafe44
Jean-Yves Vinet <jyvinet@hotmail.ca>
2017-12-02 12:19:46 -0400

    v1

--- InfoNetPm/Model/Audit.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Audit.swift
-//  InfoNetPm
-//
-//  Created by Jean-Yves Vinet on 2017-11-27.
-//  Copyright © 2017 Info JYV Inc. All rights reserved.
-//
-
-import Foundation
---
+++ InfoNetPm/New Group/Audit.swift
@@ -0,0 +1,21 @@
+//
+//  Audit.swift
+//  InfoNetPm
+//
+//  Created by jyv on 12/2/17.
+//  Copyright © 2017 Info JYV Inc. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+// Define your models like regular Swift classes
+public class Audit: Object {
+    @objc dynamic var id = UUID().uuidString
+    @objc dynamic var dateCreated = Date()
+    @objc dynamic var userCreated = ""
+    @objc dynamic var objectName = ""
+    @objc dynamic var fieldName = ""
+    @objc dynamic var fieldType = ""
+    @objc dynamic var fieldValue : Any?
+}
