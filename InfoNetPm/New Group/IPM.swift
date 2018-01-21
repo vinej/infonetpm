2b6c9fc90887d6276f6a11339a0d1a428bbb24da
Jean-Yves Vinet <jyvinet@hotmail.ca>
2018-01-21 18:00:58 -0400

    v1

---
+++ InfoNetPm/New Group/IPM.swift
@@ -0,0 +1,31 @@
+//
+//  ipm.swift
+//  InfoNetPm
+//
+//  Created by jyv on 1/21/18.
+//  Copyright © 2018 Info JYV Inc. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+public class IPM: Object {
+    @objc dynamic var id = UUID().uuidString
+    
+    // system fields
+    @objc dynamic var order = 0.0
+    @objc dynamic var createdBy = ""
+    @objc dynamic var createdDate = Date()
+    @objc dynamic var updatedBy = ""
+    @objc dynamic var updatedDate = Date()
+    
+    /* the version field is used to resolve conflits */
+    @objc dynamic var version = 0
+    /* the isSync is used to determine if a synchronization is needed for the record */
+    @objc dynamic var isSync = false
+
+    override public static func primaryKey() -> String? {
+        return "id"
+    }
+}
+
